f724e40acf77d8f49ec934aaca166a5e22ae5ea9
Breanda Barnett <bree@Breandas-iMac.local>
2022-11-22 14:35:20 +0900

    Added CSS for weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { Rings } from "react-loader-spinner";
+import "./Weather.css";
 
 export default function Weather(props) {
   function handleResponse(response) {
